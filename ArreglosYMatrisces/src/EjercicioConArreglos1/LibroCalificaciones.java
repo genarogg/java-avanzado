2a5c8904b6b3c7a8dfceff13dd2de7e8e11342f2
Genarogg <genarrogg@gmail.com>
2020-02-06 21:06:26 -0400

    LibroCalificaciones

---
+++ ArreglosYMatrisces/src/EjercicioConArreglos1/LibroCalificaciones.java
@@ -0,0 +1,21 @@
+package EjercicioConArreglos1;
+
+/**
+ *
+ * @author genarogg
+ */
+public class LibroCalificaciones {
+    private String nombreDelCurso;
+    private int [][] calificaciones;
+
+    public LibroCalificaciones(String nombreDelCurso, int[][] calificaciones) {
+        this.nombreDelCurso = nombreDelCurso;
+        this.calificaciones = calificaciones;
+    }
+
+ 
+
+    
+   
+   
+}   
