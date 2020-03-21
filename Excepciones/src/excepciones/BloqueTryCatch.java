ec2bcc10f805f5dd1bc1e450540de74506b8e6dc
Genarogg <genarrogg@gmail.com>
2020-03-20 21:26:32 -0400

    BloqueTryCatch

---
+++ Excepciones/src/excepciones/BloqueTryCatch.java
@@ -0,0 +1,17 @@
+package excepciones;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Genargg
+ */
+public class BloqueTryCatch {
+    
+    public static int cociente(int numerador, int denominador) throws ArithmeticException{
+        return numerador / denominador;
+    }
+    
+    
+}
