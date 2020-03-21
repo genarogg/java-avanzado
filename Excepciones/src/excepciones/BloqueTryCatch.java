34f4de2a4b506d71a19a6f7a753ea6a3c6a449eb
Genarogg <genarrogg@gmail.com>
2020-03-20 21:26:48 -0400

    main

--- Excepciones/src/excepciones/BloqueTryCatch.java
+++ Excepciones/src/excepciones/BloqueTryCatch.java
@@ -13,5 +13,30 @@ public class BloqueTryCatch {
         return numerador / denominador;
     }
     
-    
+    public static void main(String[] args) {
+        Scanner explorador = new Scanner(System.in);
+        boolean continuarCiclo = true;
+        
+        do{
+            try{
+                System.out.print("Introduzca un numerador entero: ");
+                int numerador = explorador.nextInt();
+                System.out.print("Introduzca un denominador entero: ");
+                int denominador = explorador.nextInt();
+                
+                int resultado = cociente(numerador, denominador);
+                System.out.printf("%nResultado: %d / %d = %d%n", numerador, denominador, resultado);
+                continuarCiclo = false;    
+                
+            }
+            catch(InputMismatchException inputMismatchException){
+                System.err.printf("%nException: %s%n", inputMismatchException);
+                explorador.nextLine();
+            }
+            catch(ArithmeticException arithmeticException){
+                System.err.printf("%nExcepcion: %s%n", arithmeticException);
+            }
+            
+        }while(continuarCiclo);
+    }
 }
