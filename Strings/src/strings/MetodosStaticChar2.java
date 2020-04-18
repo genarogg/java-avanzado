b3dc5e74cca26232fbf102dd44acaffed167cbcf
Genarogg <genarrogg@gmail.com>
2020-04-17 21:48:19 -0400

    MetodosStaticChar2

---
+++ Strings/src/strings/MetodosStaticChar2.java
@@ -0,0 +1,29 @@
+package strings;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Genarogg
+ */
+public class MetodosStaticChar2 {
+    public static void main(String [] args){
+        Scanner scanner = new Scanner (System.in);
+        
+        //obtiene la raiz
+        System.out.println("Escribe una raiz");
+        int raiz = scanner.nextInt();
+        
+        //obtiene la selecion del usuario
+        System.out.printf("Seleccione una opcion: $n1 -- %s%n2 -- %s%n",
+                          "convertir digito a caracter",
+                          "convertir caracter a digito");
+        int opcion = scanner.nextInt();
+        
+        //procesa la peticion
+       
+        
+    }
+    
+    
+}
