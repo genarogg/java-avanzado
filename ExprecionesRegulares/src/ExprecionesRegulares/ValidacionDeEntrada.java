12d2530fe63211b5b838c02aa8db4c6a6c3d246e
Genarogg <genarrogg@gmail.com>
2020-04-18 21:58:05 -0400

    ValidacionDeEntrada

---
+++ ExprecionesRegulares/src/ExprecionesRegulares/ValidacionDeEntrada.java
@@ -0,0 +1,17 @@
+package ExprecionesRegulares;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Genarogg
+ */
+public class ValidacionDeEntrada {
+    
+
+   
+    
+    public static void main(String[] args) {
+        
+    }
+}
