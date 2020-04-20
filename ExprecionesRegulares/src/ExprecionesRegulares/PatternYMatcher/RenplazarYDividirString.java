4bcec69aacb53e060d15ccfeaa2f8979853cecb0
Genarogg <genarrogg@gmail.com>
2020-04-19 22:03:05 -0400

    RenplazarYDividirString

---
+++ ExprecionesRegulares/src/ExprecionesRegulares/PatternYMatcher/RenplazarYDividirString.java
@@ -0,0 +1,19 @@
+package ExprecionesRegulares;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Genarogg
+ */
+public class RenplazarYDividirString {
+    public static void main(String[] args) {
+        String primeraCadena = "Este enunciado termina con 5 estrellas ******";
+        String segundaCadena = "1, 2, 3, 4, 5, 6, 7, 8";
+        
+        System.out.printf("Cadena 1 original: %s%n", primeraCadena);
+        
+      
+        
+    }
+}
