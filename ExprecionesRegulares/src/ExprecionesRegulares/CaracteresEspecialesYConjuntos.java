92143e0e45e2149f5ed290f1f9278256c087f627
Genarogg <genarrogg@gmail.com>
2020-04-20 22:05:46 -0400

    CaracteresEspecialesYConjuntos

---
+++ ExprecionesRegulares/src/ExprecionesRegulares/CaracteresEspecialesYConjuntos.java
@@ -0,0 +1,19 @@
+package ExprecionesRegulares;
+
+/**
+ *
+ * @author Genarogg
+ */
+
+public class CaracteresEspecialesYConjuntos {
+    public static void main(String[] args) {
+        // "\w" para letras, equivalentes "[a-zA-Z]"
+        // "\W" para no letras, equivalentes "[^a-zA-Z]"
+        // "\d" para digitos, equivalentes "[0-9]"
+        // "\D" para no digitos, equivalentes "[^0-9]"
+        // "\s" para espacios en blanco (espacios, tabuladores, etc)
+        // "\S" para no espacios en blanco
+        
+        
+    }
+}
