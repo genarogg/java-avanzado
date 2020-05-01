a70697b79d964091ce2de39e98e4ca78ec2b15db
Genarogg <genarrogg@gmail.com>
2020-05-01 14:10:35 -0400

    PruebaList

---
+++ ColeccionesDeGenericos/src/coleccionesdegenericos/PruebaList.java
@@ -0,0 +1,32 @@
+package coleccionesdegenericos;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ *
+ * @author Genarogg
+ */
+public class PruebaList {
+    public static void main(String[] args) {
+        //agrega elementos de colores a lista1
+        String [] colores = {"negro", "amarillo", "verde", "azul", "violeta", "plateado"};
+        List<String> lista1 = new LinkedList<>();
+        
+        for(String color : colores){
+            lista1.add(color);
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
+ 
+    
+   
+    
+}
