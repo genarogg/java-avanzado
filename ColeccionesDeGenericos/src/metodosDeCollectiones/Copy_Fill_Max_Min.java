d84adcaaa3c5085de5aa33df7535460bbfcfe7f3
Genarogg <genarrogg@gmail.com>
2020-05-02 14:23:49 -0400

    Copy_Fill_Max_Min

---
+++ ColeccionesDeGenericos/src/metodosDeCollectiones/Copy_Fill_Max_Min.java
@@ -0,0 +1,26 @@
+package metodosDeCollectiones;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+
+/**
+ *
+ * @author Genarogg
+ */
+public class Copy_Fill_Max_Min {
+    public static void main(String[] args) {
+        //Crea y muestra un objeto List<Character>
+        Character[] letras = {'p', 'C', 'M'};
+        List<Character> listaLetras = Arrays.asList(letras); //obtiene el objeto List
+        System.out.println("Lista contiene:");
+        imprimir(listaLetras);
+        
+       );
+        
+    }
+    
+   
+}
