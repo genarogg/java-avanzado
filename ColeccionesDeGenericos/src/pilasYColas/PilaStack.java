b7e8c11340ded9601c3af73738dabb81ce49cb7e
Genarogg <genarrogg@gmail.com>
2020-05-02 14:29:11 -0400

    PilaStack

---
+++ ColeccionesDeGenericos/src/pilasYColas/PilaStack.java
@@ -0,0 +1,34 @@
+package pilasYColas;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+/**
+ *
+ * @author Genarogg
+ */
+public class PilaStack {
+    public static void main(String[] args) {
+        Stack<Number> pila = new Stack<>();
+        
+        //usa el metodo push
+        pila.push(12L);
+        System.out.println("Se metio 12L");
+        imprimirPila(pila);
+        
+        pila.push(34567);
+        System.out.println("Se metio 34567");
+        imprimirPila(pila);
+        
+        pila.push(1.0F);
+        System.out.println("Se metio 1.0F");
+        imprimirPila(pila);
+        
+        pila.push(1234.5678);
+        System.out.println("Se metio 1234.5678");
+        imprimirPila(pila);
+        
+        
+    }
+    
+}
