7e3695013acd0b8e1aaf587ed0520da8c2896bfe
Genarogg <genarrogg@gmail.com>
2020-05-02 14:29:34 -0400

    Elimina los elementos de la pila

--- ColeccionesDeGenericos/src/pilasYColas/PilaStack.java
+++ ColeccionesDeGenericos/src/pilasYColas/PilaStack.java
@@ -28,7 +28,20 @@ public class PilaStack {
         System.out.println("Se metio 1234.5678");
         imprimirPila(pila);
         
-        
+        //Elimina los elementos de la pila
+        try{
+            Number objetoEliminado = null;
+            
+            //saca elementos de la pila
+            while(true){
+            objetoEliminado = pila.pop();
+            System.out.printf("Se saco %s%n", objetoEliminado);
+            imprimirPila(pila);
+            }
+        }
+        catch(EmptyStackException e){
+            e.printStackTrace();
+        }
     }
     
 }
