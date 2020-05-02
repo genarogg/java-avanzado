2de4eacdc07674a01d08f2f63fa611e191ff9e68
Genarogg <genarrogg@gmail.com>
2020-05-02 14:24:28 -0400

    Crea copiaLista a partir de un arreglo

--- ColeccionesDeGenericos/src/metodosDeCollectiones/Copy_Fill_Max_Min.java
+++ ColeccionesDeGenericos/src/metodosDeCollectiones/Copy_Fill_Max_Min.java
@@ -23,6 +23,15 @@ public class Copy_Fill_Max_Min {
         System.out.println("Despues de llamar a reverse, lista contiene:");
         imprimir(listaLetras);
         
+        //Crea copiaLista a partir de un arreglo
+        Character[] letrasCopia = new Character[3];
+        List<Character> copiaLista = Arrays.asList(letrasCopia);
+        
+        Collections.copy(copiaLista, listaLetras);
+        System.out.println("Despues de copiar, copiaLista contiene: ");
+        imprimir(copiaLista);
+        
+        
     }
     
    
