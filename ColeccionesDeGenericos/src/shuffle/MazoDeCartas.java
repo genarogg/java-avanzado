94e2c4975b8157efa1e41e4d1ff33bfdd272e544
Genarogg <genarrogg@gmail.com>
2020-05-04 14:41:06 -0400

    MazoDeCartas()

--- ColeccionesDeGenericos/src/shuffle/MazoDeCartas.java
+++ ColeccionesDeGenericos/src/shuffle/MazoDeCartas.java
@@ -11,7 +11,23 @@ import java.util.List;
 public class MazoDeCartas {
     private List<Carta> listaCartas; //declara objeto List que almacenará los objetos Carta
     
- 
+    //establece mazo de objetos Carta
+    public MazoDeCartas(){
+        Carta[] mazo = new Carta[52]; //un mazo tiene 52 cartas
+        int cuenta = 0; //número de cartas
+        
+        //llena el mazo con objetos Carta
+        for (Carta.Palo palo : Carta.Palo.values()) {
+            for (Carta.Cara cara : Carta.Cara.values()) { 
+                mazo[cuenta] = new Carta(cara, palo);
+                ++cuenta;
+            }
+        }
+        
+        listaCartas = Arrays.asList(mazo);
+        Collections.shuffle(listaCartas);
+    }
+    
  
     
 }
