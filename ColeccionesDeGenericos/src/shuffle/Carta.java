2a4267e26bade94fbf8d08eac442464698329e6a
Genarogg <genarrogg@gmail.com>
2020-05-04 14:39:15 -0400

    Carta

---
+++ ColeccionesDeGenericos/src/shuffle/Carta.java
@@ -0,0 +1,34 @@
+package shuffle;
+
+/**
+ *
+ * @author Genarogg
+ */
+public class Carta {
+    public static enum Cara {As, Dos, Tres, Cuatro, Cinco, Seis, Siete, Ocho, Nueve, Diez, Joker, Reina, Rey};
+    public static enum Palo {Bastos, Diamantes, Corazones, Espadas};
+    
+    private final Cara cara;
+    private final Palo palo;
+    
+    public Carta(Cara cara, Palo palo){
+        this.cara = cara;
+        this.palo = palo;
+    }
+    
+    public Cara getCara(){
+        return cara;
+    }
+    
+    public Palo getPalo(){
+        return palo;
+    }
+    
+    public String toString(){
+        return String.format("%s de %s", cara, palo);
+    }
+    
+    /*public static void main(String[] args) {
+        System.out.printf("%s", new Carta(Carta.Cara.Nueve, Carta.Palo.Diamantes));
+    }*/
+}
