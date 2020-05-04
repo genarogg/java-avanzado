534e190a6c38f6833e3b699e2428ddf3c8f4e512
Genarogg <genarrogg@gmail.com>
2020-05-04 14:40:46 -0400

    MazoDeCartas

---
+++ ColeccionesDeGenericos/src/shuffle/MazoDeCartas.java
@@ -0,0 +1,17 @@
+package shuffle;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ *
+ * @author Genarogg
+ */
+public class MazoDeCartas {
+    private List<Carta> listaCartas; //declara objeto List que almacenará los objetos Carta
+    
+ 
+ 
+    
+}
