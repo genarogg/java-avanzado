2df02c25d9fd89249cc9a1b3acf6e3e697d25c3e
Genarogg <genarrogg@gmail.com>
2020-05-04 14:37:26 -0400

    PruebaSortedSet

---
+++ ColeccionesDeGenericos/src/PruebaSortedSet/PruebaSortedSet.java
@@ -0,0 +1,37 @@
+package PruebaSortedSet;
+
+import java.util.Arrays;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ *
+ * @author Genarogg
+ */
+public class PruebaSortedSet {
+    public static void main(String[] args) {
+        //Crea un TreeSet del arreglo colores 
+        String [] colores = {"amarilo", "verde", "negro", "carne", "gris",
+                             "blanco", "naranja", "rojo", "verde"};
+        
+        SortedSet<String> arbol = new TreeSet<>(Arrays.asList(colores));
+        
+        System.out.print("Conjunto ordenado: ");
+        imprimirConjunto(arbol);
+        
+        //obtiene subconjunto mediante headSet, con base en "naranja
+        System.out.print("headSet (\"naranja\")");
+        imprimirConjunto(arbol.headSet("naranja"));
+        
+        //se obtiene un subconjunto mediante tailSet, con base en "naranja"
+        System.out.print("tailSet(\"naranja\"");
+        imprimirConjunto(arbol.tailSet("naranja"));
+        
+        //obtiene los elementos primero y ultimo
+        System.out.printf("primero: %s%n", arbol.first());
+        System.out.printf("ultimo: %s%n", arbol.last());
+        
+        }
+    
+        
+}
