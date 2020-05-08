12242247b015df603aaea9c6d0e3011b0da8d010
Genarogg <genarrogg@gmail.com>
2020-05-08 14:45:27 -0400

    constructores

--- ColeccionesDeGenericos/src/UsoDeComparator/Tiempo.java
+++ ColeccionesDeGenericos/src/UsoDeComparator/Tiempo.java
@@ -9,7 +9,17 @@ public class Tiempo {
     private int minuto;
     private int segundo;
     
+    public Tiempo(){
+        this(0, 0, 0);
+    }
+    public Tiempo(int hora){
+        this(hora, 0, 0);
+    }
+    public Tiempo(int hora, int minuto){
+        this(hora, minuto, 0);
+    }
     
+ 
 
    
     
