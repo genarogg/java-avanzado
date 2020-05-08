717d08db13b082bc578873b8cd39df665d67a3a4
Genarogg <genarrogg@gmail.com>
2020-05-08 14:45:03 -0400

    Tiempo

---
+++ ColeccionesDeGenericos/src/UsoDeComparator/Tiempo.java
@@ -0,0 +1,16 @@
+package UsoDeComparator;
+
+/**
+ *
+ * @author Genarogg
+ */
+public class Tiempo {
+    private int hora;
+    private int minuto;
+    private int segundo;
+    
+    
+
+   
+    
+}
