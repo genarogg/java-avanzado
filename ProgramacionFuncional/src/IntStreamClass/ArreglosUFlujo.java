c9e526351c840047cfdc1e1198586435eb284508
Genarogg <genarrogg@gmail.com>
2020-05-14 15:20:15 -0400

    ordena los valores en forma ascendente con flujos

--- ProgramacionFuncional/src/IntStreamClass/ArreglosUFlujo.java
+++ ProgramacionFuncional/src/IntStreamClass/ArreglosUFlujo.java
@@ -15,7 +15,12 @@ public class ArreglosUFlujo {
         //Muestra los valores originales
         System.out.printf("Valores originales: %s%n", Arrays.asList(valores));
         
-        
+        //ordena los valores en forma ascendente con flujos
+        System.out.printf("valores ordenados: %s%n",
+                        Arrays.stream(valores) //Devuelve un Stream del tipo apropiado, en este caso Stream<Integer>(Stream
+                        .sorted()//Devuelve un Stream<Integer> con los valores en orden ascendente
+                        .collect(Collectors.toList())//Se crea una nueva coleccion con los resultados
+        );
         
         
 }
