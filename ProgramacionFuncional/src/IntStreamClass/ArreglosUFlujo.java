2bd29ddc4d1c9791f89109b24824407637cb4d03
Genarogg <genarrogg@gmail.com>
2020-05-14 15:19:52 -0400

    ArreglosUFlujo

---
+++ ProgramacionFuncional/src/IntStreamClass/ArreglosUFlujo.java
@@ -0,0 +1,21 @@
+package IntStreamClass;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author Genarogg
+ */
+public class ArreglosUFlujo {
+    public static void main(String[] args) {
+        Integer[] valores = {2, 9, 5, 0, 3, 7, 1, 4, 8, 6};
+        
+        //Muestra los valores originales
+        System.out.printf("Valores originales: %s%n", Arrays.asList(valores));
+        
+        
+        
+        
+}
