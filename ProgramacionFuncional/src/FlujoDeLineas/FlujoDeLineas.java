69ec9bb065edef680502d0561a0dc0e6b01efef2
Genarogg <genarrogg@gmail.com>
2020-05-14 15:12:38 -0400

    FlujoDeLineas

---
+++ ProgramacionFuncional/src/FlujoDeLineas/FlujoDeLineas.java
@@ -0,0 +1,32 @@
+package FlujoDeLineas;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author Genarogg
+ */
+public class FlujoDeLineas {
+    public static void main(String[] args) {
+        //Expresion regular que coincide con uno o mas caracteres consecutivos
+        //de espacio en blanco
+        
+        Pattern patron = Pattern.compile("\\s+");
+        
+        //cuenta las ocurrencias de cada palabra en un Stream<String> ordenado
+        //por palabra
+        
+        
+        
+      
+   
+        
+        
+    }
+}
