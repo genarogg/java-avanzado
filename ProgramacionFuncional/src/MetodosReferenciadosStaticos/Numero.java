b0a5588ca439be00a8353179261000bda61a5041
Genarogg <genarrogg@gmail.com>
2020-05-15 15:23:14 -0400

    Numero

---
+++ ProgramacionFuncional/src/MetodosReferenciadosStaticos/Numero.java
@@ -0,0 +1,25 @@
+package MetodosReferenciadosStaticos;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiPredicate;
+
+/**
+ *
+ * @author Genarogg
+ */
+public class Numero {
+    /**
+     * metodo estatico que nos devuelve true si la suma de ambos numeros es mayor que cincuenta
+     */
+    
+    public static boolean esMayorQueCincuenta(int n1, int n2){
+        return (n1 + n2) < 50;
+    }
+    
+
+    
+    
+  
+}
