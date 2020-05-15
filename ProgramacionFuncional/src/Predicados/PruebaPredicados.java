450a780eb3e1ce268e8bb63d3d24941933ddef7b
Genarogg <genarrogg@gmail.com>
2020-05-15 15:28:04 -0400

    PruebaPredicados

---
+++ ProgramacionFuncional/src/Predicados/PruebaPredicados.java
@@ -0,0 +1,15 @@
+package Predicado.Predicados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ *
+ * @author Genarogg
+ */
+public class PruebaPredicados {
+    public static void main(String[] args) {
+       
+    }   
+}
