f6b72fb8f8ef31b66e275f3428fe9ed3b28b75ed
Genarogg <genarrogg@gmail.com>
2020-05-15 15:28:22 -0400

    PruebaPredicados main

--- ProgramacionFuncional/src/Predicados/PruebaPredicados.java
+++ ProgramacionFuncional/src/Predicados/PruebaPredicados.java
@@ -10,6 +10,19 @@ import java.util.function.Predicate;
  */
 public class PruebaPredicados {
     public static void main(String[] args) {
+        List<Persona> listaPersonas = new ArrayList<>();
+        Persona p1 = new Persona ("pepe", "perez", 20);
+        Persona p2 = new Persona ("angel", "sanchez", 30);
+        Persona p3= new Persona ("pepe", "blanco", 40);
+        
+        listaPersonas.add(p1);
+        listaPersonas.add(p2);
+        listaPersonas.add(p3);
+        
+        //recorremos la lista por medio de un Stream
+        listaPersonas.stream().forEach(p -> System.out.println(p.getNombre()));
+        
+        System.out.println();
        
     }   
 }
