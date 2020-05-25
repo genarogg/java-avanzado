d527bfc29bdf17e0b5819b50def27b2da6f30348
Genarogg <genarrogg@gmail.com>
2020-05-24 23:07:57 -0400

    almacina valores del 1 al 10 en el buffer sharedLocation

--- concurrencia/src/BlockingQueue/Producer.java
+++ concurrencia/src/BlockingQueue/Producer.java
@@ -19,6 +19,25 @@ public class Producer implements Runnable{
 
    
     
-   
+    //almacina valores del 1 al 10 en el buffer sharedLocation
+    @Override
+    public void run() {
+        int sum = 0;
+        
+        for(int count = 1; count <= 10; count++){
+            try {
+                Thread.sleep(generador.nextInt(3000));
+                SHAREDlOCATION.blockingPut(count);
+                sum += count;
+                System.out.printf("\t%2d%n", sum);
+                
+            }
+            catch (Exception e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        System.out.printf("el productor realizo la produccion %n Terminando Productor%n ");
+        
+    }
     
 }
