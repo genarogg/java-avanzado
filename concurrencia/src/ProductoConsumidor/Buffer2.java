06d1931d9fcc7ebee6f9d9414da76922029055e6
Genarogg <genarrogg@gmail.com>
2020-05-25 23:19:37 -0400

    recoger

--- concurrencia/src/ProductoConsumidor/Buffer2.java
+++ concurrencia/src/ProductoConsumidor/Buffer2.java
@@ -7,7 +7,21 @@ package ProductoConsumidor;
 public class Buffer2 {
     private char contenido;
     private boolean disponible = false;
-   
+    
+    public synchronized char recoger(){
+        while(disponible == false){
+            try {
+                wait();
+            }
+            catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+        
+        disponible = false;
+        notify();
+        return contenido;
+    }
     
    
 }
