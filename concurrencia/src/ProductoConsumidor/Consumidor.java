93704d28533cbfb06b1a8b7aa4fef31296b19875
Genarogg <genarrogg@gmail.com>
2020-05-25 23:22:32 -0400

    run()

--- concurrencia/src/ProductoConsumidor/Consumidor.java
+++ concurrencia/src/ProductoConsumidor/Consumidor.java
@@ -19,5 +19,20 @@ public class Consumidor implements Runnable {
     
     
     
+    @Override
+    public void run() {
+        char valor;
+        
+        for(int i = 0; i < 10; i++){
+            valor = buffer.recoger();
+            System.out.println(i + " Consumidor: " + valor);
+            try {
+                Thread.sleep(100);
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     
 }
