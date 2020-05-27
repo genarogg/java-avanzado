0e57bdecba168e1e8e504704ddc08e8db80bc57c
Genarogg <genarrogg@gmail.com>
2020-05-26 23:14:04 -0400

    Realizar calculos por separado

--- concurrencia/src/StreamsParalelos/TestStreams.java
+++ concurrencia/src/StreamsParalelos/TestStreams.java
@@ -23,7 +23,16 @@ public class TestStreams {
         //crear array aleatorio de valores tipo long
         long[] values = random.longs(10_000_000, 1, 1001).toArray();
         
-       
+        //Realizar calculos por separado
+        Instant separateStart = Instant.now();
+        long count = Arrays.stream(values).count();
+        long sum = Arrays.stream(values).sum();
+        long min = Arrays.stream(values).min().getAsLong();
+        long max = Arrays.stream(values).max().getAsLong();
+        double average = Arrays.stream(values).average().getAsDouble();
+        Instant separateEnd = Instant.now();
+        
+        
     }
 
     
